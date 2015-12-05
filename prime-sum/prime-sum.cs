6a2a53be9cb603646d201c594a71e77fec454aa6
Michael A. Smith <msmith3@ebay.com>
2015-12-05 16:10:55 -0500

    Prime Sum (Naive Prime Finder)

---
+++ prime-sum/prime-sum.cs
@@ -0,0 +1,27 @@
+using System.IO;
+
+class Program {
+    static bool isPrime(int i) {
+        int j;
+        if (i % 2 == 0) return false;
+        for (j = 3; j * j < i; j += 2) {
+            if (i % j == 0) return false;
+        }
+        return true;
+    } 
+
+    static void Main(string[] args) {
+        // Print to stdout the sum of the first 1000 prime numbers.
+        int sum = 2;
+        int i = 3;
+        int foundPrimes = 1;
+        while (foundPrimes < 1000) {
+            if (isPrime(i)) {
+                sum += i;
+                foundPrimes += 1;
+            }
+            i += 1;
+        }
+        System.Console.WriteLine(sum);
+    }
+}
